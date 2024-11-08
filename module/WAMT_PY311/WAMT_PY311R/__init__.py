648ca0f0bd8c21adcac44169d29849bd0ae80fa0
BotJimmy2006 <phuphaeiei55@gmail.com>
2024-11-08 23:30:59 +0700

    first commit

---
+++ module/WAMT_PY311/WAMT_PY311R/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.10 (pro), 005845, 2024-08-13T04:15:47.795239
+from .pyarmor_runtime import __pyarmor__
---
+++ module/WAMT_PY312/WAMT_PY312R/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.11 (pro), 005845, 2024-08-13T06:18:21.943966
+from .pyarmor_runtime import __pyarmor__
